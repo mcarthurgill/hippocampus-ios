675a27cfbf2e293444952d5a1729f5183c8ecb54
Billy <william@busproductions.com>
2014-06-18 11:49:20 -0400

    Initial Commit

---
+++ Hippocampus/LXAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LXAppDelegate.h
+//  Hippocampus
+//
+//  Created by Will Schreiber on 6/18/14.
+//  Copyright (c) 2014 LXV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LXAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Hippocampus/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Hippocampus
+//
+//  Created by Will Schreiber on 6/18/14.
+//  Copyright (c) 2014 LXV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LXAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LXAppDelegate class]));
+    }
+}
