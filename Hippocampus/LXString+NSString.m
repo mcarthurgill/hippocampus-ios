e4009880e76ea96197ce630738aaf39dcdd6826a
mcarthurgill <mickgill2008@gmail.com>
2015-03-10 09:52:04 -0500

    awesome note! notis

--- Hippocampus/LXString+NSString.m
+++ Hippocampus/LXString+NSString.m
@@ -15,4 +15,9 @@
     return length < [self length] ? [NSString stringWithFormat:@"%@ [...]", [self substringWithRange:NSMakeRange(0, length)]] : self;
 }
 
++ (NSString *) randomCongratulations {
+    NSArray *arr = [NSArray arrayWithObjects:@"Awesome Note!", @"Not going to forget that!", @"Way to go!", @"You're crushing it!", @"Nice memory!", @"You're getting smarter!", @"Great memory!", @"It's the details that matter", @"Memory game strong!", nil];
+    return [arr objectAtIndex:arc4random_uniform((uint32_t)[arr count])];
+}
+
 @end
