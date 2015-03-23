95e97a1be9bfd75ae9c835be414e1ed251715271
w <w@lxv.io>
2015-03-23 15:03:28 -0500

    changes

---
+++ Hippocampus/UIPickerView+CustomPicker.h
@@ -0,0 +1,16 @@
+//
+//  UIPickerView+CustomPicker.h
+//  Hippocampus
+//
+//  Created by Will Schreiber on 3/18/15.
+//  Copyright (c) 2015 LXV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIPickerView (CustomPicker)
+
+- (BOOL) isDayPicker;
+- (BOOL) isTypePicker;
+
+@end
