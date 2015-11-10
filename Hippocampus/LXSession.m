071a1367ab67a2f04f094a5bc0e8367db73b158c
w <w@lxv.io>
2015-11-09 18:51:25 -0600

    remove hippo

--- Hippocampus/LXSession.m
+++ Hippocampus/LXSession.m
@@ -69,7 +69,8 @@ static LXSession* thisSession = nil;
     if (temp && ![[temp localKey] isEqualToString:[LXObjectManager objectWithLocalKey:@"localUserKey"]]) {
         [LXObjectManager assignLocal:[temp localKey] WithLocalKey:@"localUserKey" alsoToDisk:YES];
     } else if (temp) {
-        [[[NSUserDefaults alloc] initWithSuiteName: @"group.busproductions.HippocampusSharingDefaults"] setObject:temp forKey:@"user"];
+        [[[NSUserDefaults alloc] initWithSuiteName: @"group.busproductions.HippocampusSharingDefaults"] setObject:[temp mutableCopy] forKey:@"user"];
+        //NSLog(@"TEMP!: %@", temp);
     }
     return temp;
     if (self.cachedUser) {
